741fd641ba98c78cefc36010022b332f89bf4c9a
ken <kenjutsu@protonmail.com>
2018-08-27 12:32:27 -0500

    Created tasks scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180820002147) do
+ActiveRecord::Schema.define(version: 20180827172405) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
